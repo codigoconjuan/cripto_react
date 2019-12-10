159984f087e3acba7ea3a67a898ffce0df9cf679
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-10 17:59:33 -0600

    finalizando useCriptomoneda

--- src/hooks/useCriptomoneda.js
+++ src/hooks/useCriptomoneda.js
@@ -23,7 +23,7 @@ const Select = styled.select`
 
 const useCriptomoneda = (label, stateInicial, opciones) => {
 
-    console.log(opciones);
+    // console.log(opciones);
 
     // State de nuestro custom hook
     const [state, actualizarState] = useState(stateInicial);
@@ -36,9 +36,9 @@ const useCriptomoneda = (label, stateInicial, opciones) => {
                 value={state}
             >
                 <option value="">- Seleccione -</option>
-                {/* {opciones.map(opcion => (
-                    <option key={opcion.codigo} value={opcion.codigo}>{opcion.nombre}</option>
-                ))} */}
+                {opciones.map(opcion => (
+                    <option key={opcion.CoinInfo.Id} value={opcion.CoinInfo.Name}>{opcion.CoinInfo.FullName}</option>
+                ))}
             </Select>
         </Fragment>
     );
