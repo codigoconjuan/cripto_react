3cdb12f54f05429b1d2b102fb9592b05ed23cc8c
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-10 17:37:47 -0600

    finalizando el hook de useMoneda

--- src/hooks/useMoneda.js
+++ src/hooks/useMoneda.js
@@ -1,4 +1,25 @@
 import React, { Fragment, useState } from 'react';
+import styled from '@emotion/styled';
+
+const Label = styled.label`
+    font-family: 'Bebas Neue', cursive;
+    color: #FFF;
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 2.4rem;
+    margin-top: 2rem;
+    display: block;
+`;
+
+const Select = styled.select`
+    width: 100%;
+    display:block;
+    padding: 1rem;
+    -webkit-appearance: none;
+    border-radius: 10px;
+    border: none;
+    font-size: 1.2rem;
+`
 
 const useMoneda = (label, stateInicial, opciones) => {
 
@@ -7,8 +28,8 @@ const useMoneda = (label, stateInicial, opciones) => {
 
     const Seleccionar = () => (
         <Fragment>
-            <label>{label}</label>
-            <select
+            <Label>{label}</Label>
+            <Select
                 onChange={ e => actualizarState(e.target.value)}
                 value={state}
             >
@@ -16,7 +37,7 @@ const useMoneda = (label, stateInicial, opciones) => {
                 {opciones.map(opcion => (
                     <option key={opcion.codigo} value={opcion.codigo}>{opcion.nombre}</option>
                 ))}
-            </select>
+            </Select>
         </Fragment>
     );
 
