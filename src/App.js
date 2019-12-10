3c0584a2e8f45b5ed6673ae77b6d248132093e07
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-09 19:53:41 -0600

    agregando el diseño

--- src/App.js
+++ src/App.js
@@ -1,8 +1,52 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import imagen from './cryptomonedas.png';
+
+const Contenedor = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  @media (min-width:992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`;
+
+const Imagen = styled.img`
+  max-width: 100%;
+  margin-top: 5rem;
+`;
+const Heading = styled.h1`
+  font-family: 'Bebas Neue', cursive;
+  color: #FFF;
+  text-align:left;
+  font-weight: 700;
+  font-size: 50px;
+  margin-bottom: 50px;
+  margin-top: 80px;
+
+  &::after {
+    content: '';
+    width: 100px;
+    height: 6px;
+    background-color: #66A2FE;
+    display:block;
+  }
+`;
 
 function App() {
   return (
-    <h1>Cotizador de Criptomonedas</h1>
+    <Contenedor>
+        <div>
+          <Imagen 
+            src={imagen}
+            alt="imagen cripto"
+          />
+        </div>
+        <div>
+            <Heading>Cotiza Criptomonedas al Instante</Heading>
+        </div>
+    </Contenedor>
   );
 }
 
