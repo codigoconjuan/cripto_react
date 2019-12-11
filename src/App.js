06e209b5a4a6db0f2664817890b5784f3b230937
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-10 18:24:49 -0600

    cotizando en la api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import styled from '@emotion/styled';
+import axios from 'axios';
 import imagen from './cryptomonedas.png';
 import Formulario from './components/Formulario'
 
@@ -40,14 +41,20 @@ function App() {
   const [moneda, guardarMoneda] = useState('');
   const [criptomoneda, guardarCriptomoneda] = useState('');
 
-  useEffect(() => {
+  useEffect( () => {
 
-    // evitamos la ejecución la primera vez
-    if(moneda === '') return;
+      const cotizarCriptomoneda = async () => {
+          // evitamos la ejecución la primera vez
+          if(moneda === '') return;
 
-    
+          // consultar la api para obtener la cotizacion
+          const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
 
+          const resultado = await axios.get(url);
 
+          console.log(resultado.data.DISPLAY[criptomoneda][moneda] );
+      }
+      cotizarCriptomoneda();
   }, [moneda, criptomoneda]);
 
   return (
