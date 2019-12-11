d6df295cd442a8bffdf9dd3f56cd48c3c9c26cc4
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-10 18:09:54 -0600

    mostrando el mensaje de error

---
+++ src/components/Error.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const MensajeError = styled.p`
+    background-color: #b7322c;
+    padding: 1rem;
+    color: #FFF;
+    font-size: 30px;
+    text-transform: uppercase;
+    font-weight: bold;
+    text-align: center;
+    font-family: 'Bebas Neue', cursive;
+`;
+
+const Error = ({mensaje}) => {
+    return (  
+        <MensajeError>{mensaje}</MensajeError>
+    );
+}
+ 
+export default Error;
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import styled from '@emotion/styled';
 
+import Error from './Error';
 import useMoneda from '../hooks/useMoneda';
 import useCriptomoneda from '../hooks/useCriptomoneda';
 import axios from 'axios';
@@ -73,7 +74,7 @@ const Formulario = () => {
         <form
             onSubmit={cotizarMoneda}
         >
-            {error ? 'Hay un error': null}
+            {error ? <Error mensaje="Todos los campos son obligatorios" /> : null}
 
             <SelectMonedas />
 
